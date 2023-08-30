e9b5601827bb6311b95589231836ae61d7dfb884
rex69420 <65942753+rex69420@users.noreply.github.com>
2023-08-30 11:04:07 +0530

    fix form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,12 +1,10 @@
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting normally
 
-    // Get the value of the text input
     var inputValue = document.querySelector('#textbox').value;
 
-    // Check if the input value is equal to a particular input
     if (inputValue === 'DADA-CON42') {
-        window.location.href = 'otherindex.html'; // Redirect to another website
+        window.location.href = 'otherIndex.html';
     }
 });
 
