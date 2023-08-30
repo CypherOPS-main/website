91bb7f48a6f2e2910206d6e56eac8a24aa500e78
itkash007 <it.kash.007@gmail.com>
2023-08-30 10:45:22 +0530

    form done

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,12 @@
-    const textbox = document.querySelector('#textbox');
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get the value of the text input
+    var inputValue = document.querySelector('#textbox').value;
+
+    // Check if the input value is equal to a particular input
+    if (inputValue === 'DADA-CON23') {
+        window.location.href = 'otherindex.html'; // Redirect to another website
+    }
+});
+
